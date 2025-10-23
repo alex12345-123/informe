45eece12cd1d0ba2d46c01b295a2251ca41c2321
Alex <alex.tashiguano@educa.madrid.org>
2025-10-23 04:29:19 -0500

    v3 informes

--- informes/src/main/java/es/etg/dam/Lanzador.java
+++ informes/src/main/java/es/etg/dam/Lanzador.java
@@ -2,5 +2,10 @@ package es.etg.dam;
 
 public class Lanzador {
 
+    private static final String [] COMANDOS ={"ps", "df", "free" };
     
+    public static void main(String[] args) {
+        
+    }
+
 }
---
+++ informes/src/main/java/es/etg/dam/generarInforme.java
@@ -0,0 +1,21 @@
+package es.etg.dam;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class generarInforme {
+    public static final String INFORME = "informe.md";
+    public void guardarInforme (String contenido) {
+        try(PrintWriter pw = new PrintWriter(new FileWriter(INFORME, true))){
+            pw.println(contenido);
+            pw.println("\n---\n");
+
+
+        } catch (IOException e) {
+            System.out.println("Error al guardar el informe");
+
+
+        }
+    }
+}
